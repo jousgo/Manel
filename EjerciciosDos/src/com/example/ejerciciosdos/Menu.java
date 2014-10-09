8d5ef731921053425ebb4999280e43d6d0f39dc4
jousgo <jousgo@gmail.com>
2014-10-09 20:55:07 +0200

    2

--- EjerciciosDos/src/com/example/ejerciciosdos/Menu.java
+++ EjerciciosDos/src/com/example/ejerciciosdos/Menu.java
@@ -1,9 +1,10 @@
 package com.example.ejerciciosdos;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class Menu extends Activity {
 
@@ -11,24 +12,16 @@ public class Menu extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu);
+		
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu, menu);
-		return true;
+	
+	public void IrEje1 (View mostrar){
+		Intent ejer1 = new Intent(this, Ejer1.class);
+		startActivity(ejer1);
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	
+	public void IrEje2 (View mostrar){
+		Intent ejer2 = new Intent(this, Ejer2.class);
+		startActivity(ejer2);
 	}
 }
